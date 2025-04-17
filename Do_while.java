0fc479055200b0c30f014b374f60d8fb9714b3e3
Aman Raj <636amanraj@gmail.com>
2025-04-18 00:32:02 +0530

    Add files via upload

---
+++ Do_while.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Do_while
+{
+    public static void main(String[] args)
+    {
+        int a = 0;
+        do {
+            System.out.println(a);
+        }
+        while(a<=10);
+
+    }
+}
